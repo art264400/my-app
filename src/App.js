cc24034dfb7fa7e6812ed45789f22346641e2dae
Артур Губайдуллин <gubaydullin@lanit.ru>
2021-09-20 16:24:56 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <p>App</p>
+      <body>
+      </body>
     </div>
+    <div><header /></div>
   );
+}
+
+function header(){
+    return (<div>
+    <ul>
+        <li>ѹ</li>
+        <li>ANGULAR</li>
+        <li>REACT</li>
+        </ul> </div>)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import header from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

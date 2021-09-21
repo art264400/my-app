b17c5f17673d20af3f7121458344d381359c13b2
Артур Губайдуллин <gubaydullin@lanit.ru>
2021-09-21 17:29:47 +0500

    закончил 19, route

---
+++ src/components/Dialogs/Diaglogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import c from './Diaglogs.module.css'
+
+
+const Diaglogs = () => {
+    return (
+      <div className={c.content}>
+        Dialogsdasasddas
+      </div>
+    )
+}
+export default Diaglogs;
